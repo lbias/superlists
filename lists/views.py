af65b6b2b994c527507d0d08d5babb0929616798
lbias <306508354@qq.com>
2017-09-27 22:50:54 +0800

    make view discriminate over which items it sends to the template

--- lists/views.py
+++ lists/views.py
@@ -6,7 +6,8 @@ def home_page(request):
     return render(request, 'home.html')
 
 def view_list(request, list_id):
-    items = Item.objects.all()
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
