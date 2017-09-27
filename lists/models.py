939e78d3e96c1c3e71e50d727cdc51b28742cdea
lbias <306508354@qq.com>
2017-09-27 22:22:25 +0800

    Item/List relationship

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class Item(models.Model):
-    text = models.TextField(default='')
-    list = models.TextField(default='')
 
 class List(models.Model):
     pass
+
+class Item(models.Model):
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
