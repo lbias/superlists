3e2a92f7cab7d9e0d219b3115d90bc9458c8af9f
lbias <306508354@qq.com>
2017-09-29 23:28:38 +0800

    existing list item form

--- lists/forms.py
+++ lists/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.models.ModelForm):
     class Meta:
@@ -21,3 +22,7 @@ class ItemForm(forms.models.ModelForm):
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
+
+class ExistingListItemForm(ItemForm):
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
