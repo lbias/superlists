87d95929226d72b69a99837a89da36b4c8550d88
lbias <306508354@qq.com>
2017-09-29 15:32:15 +0800

    use EMPTY_ITEM_ERROR in test_form

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 class ItemFormTest(TestCase):
     def test_form_renders_item_text_input(self):
@@ -15,7 +15,4 @@ class ItemFormTest(TestCase):
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.errors['text'],
-            ["You can't have an empty list item"]
-        )
+        self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
