33d18a0de6d2c68b44d46e43ff07d841c2bd32bc
lbias <306508354@qq.com>
2017-09-29 15:12:32 +0800

    add class to ItemForm

--- lists/forms.py
+++ lists/forms.py
@@ -4,5 +4,6 @@ class ItemForm(forms.Form):
     item_text = forms.CharField(
         widget=forms.fields.TextInput(attrs={
             'placeholder': 'Enter a to-do item',
+            'class': 'form-control input-lg',
         }),
     )
